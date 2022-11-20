6479620d2cad06505f3cc13dbc0838fbf63c5fdb
Shashank Agrawal <shashank@letscooee.com>
2022-11-20 18:41:08 +0530

    Do not show commits from all the branches

--- aliases/git.sh
+++ aliases/git.sh
@@ -55,8 +55,8 @@ alias gstashes='git stash list'
 
 # Log related
 alias gl='git log'
-alias gl1="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
-alias gl2="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ad)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias gl1="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)'"
+alias gl2="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ad)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)'"
 alias gl3="git log --pretty=format:'%Cred%ad%Creset %C(yellow)%h%Creset by %C(bold blue)%an%Creset, %s' --date=iso | sort -r | less"
 alias glone='git log --graph --pretty=oneline --abbrev-commit'
 
