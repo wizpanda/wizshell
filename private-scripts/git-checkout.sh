6f9028c70eebfc548d64bd298a4dff63d1cab7e5
Shashank Agrawal <shashank@letscooee.com>
2025-10-15 12:05:30 +0530

    Add force flag to "gc" alias to handle branch cleanup before pulling

--- private-scripts/git-checkout.sh
+++ private-scripts/git-checkout.sh
@@ -1,17 +1,65 @@
 #!/bin/bash -e
 
-BRANCH_OR_TAG=$1
+BRANCH_OR_TAG=""
 REMOTE_NAME=origin
 GIT_FETCH_ONCE_DONE=false
+FORCE_FLAG=false
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -f|--force)
+            FORCE_FLAG=true
+            shift
+            ;;
+        *)
+            if [[ -z "$BRANCH_OR_TAG" ]]; then
+                BRANCH_OR_TAG=$1
+            else
+                REMOTE_NAME=$1
+            fi
+            shift
+            ;;
+    esac
+done
 
 if [[ ! ${BRANCH_OR_TAG} ]]; then
     echo -e "${RED}First argument missing i.e. tag or branch name ${NONE}"
     exit 1
 fi
 
-# "gc android-create-channel origin2"
-if [[ $2 ]]; then
-    REMOTE_NAME=$2
+# Handle force flag - delete local branch and remote reference
+if [[ ${FORCE_FLAG} == true ]]; then
+    echo -e "${YELLOW}Force flag detected. Cleaning up existing references...${NONE}"
+
+    # Check if currently on the branch we want to delete
+    currentBranch=$(git branch --show-current)
+    if [[ "$currentBranch" == "$BRANCH_OR_TAG" ]]; then
+        echo -e "${BLUE}Currently on branch [$BRANCH_OR_TAG]. Switching to main/master first...${NONE}"
+        git checkout main 2>/dev/null || git checkout master 2>/dev/null || {
+            echo -e "${RED}Cannot switch away from current branch. Please checkout to a different branch first.${NONE}"
+            exit 1
+        }
+    fi
+
+    # Delete local branch if it exists
+    localReference=$(git rev-parse -q --verify ${BRANCH_OR_TAG}) || true
+    if [[ "$localReference" != "" ]]; then
+        echo -e "${BLUE}Deleting local branch [$BRANCH_OR_TAG]...${NONE}"
+        git branch -D ${BRANCH_OR_TAG} || true
+    fi
+
+    # Delete remote tracking reference if it exists
+    remoteRef=$(git show-ref refs/remotes/${REMOTE_NAME}/${BRANCH_OR_TAG}) || true
+    if [[ "$remoteRef" != "" ]]; then
+        echo -e "${BLUE}Deleting remote tracking reference [${REMOTE_NAME}/${BRANCH_OR_TAG}]...${NONE}"
+        git branch -rd ${REMOTE_NAME}/${BRANCH_OR_TAG} || true
+    fi
+
+    # Fetch fresh from remote
+    echo -e "${BLUE}Fetching fresh from remote [$REMOTE_NAME]...${NONE}"
+    git fetch ${REMOTE_NAME}
+    GIT_FETCH_ONCE_DONE=true
 fi
 
 localReference=$(git rev-parse -q --verify ${BRANCH_OR_TAG}) || true
